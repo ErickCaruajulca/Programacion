f03cbc037015f21854198a8fa467c24dc84d0777
Franco <N00398622@upn.pe>
2026-04-24 23:56:06 -0500

    agregue la complejidad

--- ExamenT1.py
+++ ExamenT1.py
@@ -1,1 +1,10 @@
 print ("Hola mundo")
+print("Complejidad algorítmica:")
+print("T(n) = n + 3 + n(n-1)/2")
+
+print("\nNotación Big O:")
+print("O(n^2)")
+
+print("\nRespuesta final:")
+print("La complejidad del algoritmo es CUADRÁTICA")
+print("porque tiene dos ciclos for anidados.")
